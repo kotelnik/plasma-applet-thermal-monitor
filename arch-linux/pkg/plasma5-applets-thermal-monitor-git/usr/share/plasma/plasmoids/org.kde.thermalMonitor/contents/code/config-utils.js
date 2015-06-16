df4aaa4ca5872440dd8337d2d6d2c6488cf0d466
Kotelnik <clearmartin@seznam.cz>
2015-06-16 17:18:32 +0200

    handling hddtemp and nvidia-sni fixed

--- arch-linux/pkg/plasma5-applets-thermal-monitor-git/usr/share/plasma/plasmoids/org.kde.thermalMonitor/contents/code/config-utils.js
+++
@@ -1,8 +0,0 @@
-function getResourcesObjectArray() {
-    var cfgResources = plasmoid.configuration.resources
-    print('Reading resources from configuration: ' + cfgResources)
-    if (!cfgResources) {
-        return []
-    }
-    return JSON.parse(cfgResources)
-}
